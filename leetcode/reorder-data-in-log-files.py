41a783c77de04aaadd87e291f09d7edee487fd63
apeltop <sunshine@ptokos.com>
2021-02-15 20:05:56 +0900

    leetcode reorder-data-in-log-files 문제 풀이 추가하였다.

---
+++ leetcode/reorder-data-in-log-files.py
@@ -0,0 +1,21 @@
+# 파이썬 알고리즘 인터뷰 책 참고 p.149
+from typing import List
+
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
+
+
+print(Solution().reorderLogFiles(["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig",
+                                  "let3 art zero"]))  # ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+print(Solution().reorderLogFiles(["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog",
+                                  "a8 act zoo"]))  # ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
