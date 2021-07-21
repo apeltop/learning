15461e8fa41589edfa5ee3ac7dd128ab1aa6b090
psycho3081 <psycho3081@dgsw.hs.kr>
2021-07-22 00:32:37 +0900

    웹 페이지 다운로드 simple 버전 코드 작성하였다.

---
+++ book/파이썬_웹_스크래핑/chapter1/p39_html_download.py
@@ -0,0 +1,16 @@
+import urllib.request
+from urllib.error import URLError, HTTPError, ContentTooShortError
+
+
+def download(url):
+    print(f'Downloading: {url}')
+    try:
+        html = urllib.request.urlopen(url).read()
+    except (URLError, HTTPError, ContentTooShortError) as e:
+        print(f'Download error: {e.reason}')
+        html = None
+
+    return html
+
+
+print(download('https://www.google.co.kr'))
