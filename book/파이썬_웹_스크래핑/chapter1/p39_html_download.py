100f7c67744124c3f5831d63191c666d2661b03d
psycho3081 <psycho3081@dgsw.hs.kr>
2021-07-22 10:17:38 +0900

    웹 페이지 다운로드 시 Response Code 500 반환 시 3번 더 시도하는 코드 작성

--- book/파이썬_웹_스크래핑/chapter1/p39_html_download.py
+++ book/파이썬_웹_스크래핑/chapter1/p39_html_download.py
@@ -2,15 +2,22 @@ import urllib.request
 from urllib.error import URLError, HTTPError, ContentTooShortError
 
 
-def download(url):
+def download(url, user_agent='wswp', num_retries=2):
     print(f'Downloading: {url}')
+
+    request = urllib.request.Request(url)
+    request.add_header('User-agent', user_agent)
+
     try:
-        html = urllib.request.urlopen(url).read()
+        html = urllib.request.urlopen(request).read()
     except (URLError, HTTPError, ContentTooShortError) as e:
-        print(f'Download error: {e.reason}')
+        print(f'Download error: {e.code} {e.reason}')
         html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                return download(url, user_agent, num_retries - 1)
 
     return html
 
 
-print(download('https://www.google.co.kr'))
+print(download('https://httpstat.us/500'))
