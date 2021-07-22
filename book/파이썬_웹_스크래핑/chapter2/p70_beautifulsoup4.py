f33e9c534fa4e83ab82af799ddde24be8e9cb50a
psycho3081 <psycho3081@dgsw.hs.kr>
2021-07-22 14:56:11 +0900

    html5lib 을 사용하여 제대로 닫히지 않는 태그 닫히도록 하였다. 추후 크롤링 시 항상 html 다운받고 html5lib 사용해야겠다.

--- book/파이썬_웹_스크래핑/chapter2/p70_beautifulsoup4.py
+++ book/파이썬_웹_스크래핑/chapter2/p70_beautifulsoup4.py
@@ -6,3 +6,9 @@ broken_html = '<ul class=country><li>Area<li>Population</ul>'
 soup = BeautifulSoup(broken_html, 'html.parser')
 fixed_html = soup.prettify()
 pprint(fixed_html)
+
+print()
+
+soup = BeautifulSoup(broken_html, 'html5lib')
+fixed_html = soup.prettify()
+pprint(fixed_html)
