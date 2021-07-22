62ae2bc44ff83718f51e8da293a882f7012791d7
apeltop <sunshine@ptokos.com>
2021-07-22 23:25:15 +0900

    구글 브라우저가 열리고 11을 검색 후 브라우저 닫히는 코드 작성

--- book/파이썬_웹_스크래핑/chapter5/p164_google_open.py
+++ book/파이썬_웹_스크래핑/chapter5/p164_google_open.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(executable_path='../driver/chromedriver')
 driver.get('https://www.google.com')
+
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('11')
+driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.RETURN)
+
+driver.close()
