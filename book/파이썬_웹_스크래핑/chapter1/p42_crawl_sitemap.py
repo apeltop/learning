d82f09b5b53accd28cf6e73b65b1b82da94ddc93
psycho3081 <psycho3081@dgsw.hs.kr>
2021-07-22 10:30:58 +0900

    sitemap.xml 을 이용하여 loc 안에 있는 url 다운로드 받는 코드 작성

---
+++ book/파이썬_웹_스크래핑/chapter1/p42_crawl_sitemap.py
@@ -0,0 +1,37 @@
+import re
+import urllib.request
+from urllib.error import ContentTooShortError, HTTPError, URLError
+
+
+def download(url, num_retries=2, user_agent='wswp', charset='utf-8'):
+    print(f'Downloading: {url}')
+
+    request = urllib.request.Request(url)
+    request.add_header('User-agent', user_agent)
+
+    try:
+        resp = urllib.request.urlopen(request)
+        cs = resp.headers.get_content_charset()
+        if not cs:
+            cs = charset
+        html = resp.read().decode(cs)
+    except (URLError, HTTPError, ContentTooShortError) as e:
+        print(f'Download error: {e.reason}')
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                return download(url, num_retries - 1)
+
+    return html
+
+
+def crawl_sitemap(url):
+    sitemap = download(url)
+
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+
+    for link in links:
+        html = download(link)
+
+
+crawl_sitemap('https://www.eebew.com/sitemap.xml')
