d59e2ee7cd5b6cf4f37f878c4dcc351488ffa6ee
apeltop <sunshine@ptokos.com>
2022-01-18 01:16:45 +0900

    console 에 error 출력 시 throw 되도록 하는 기능 추가

--- book/리액트_프로그래밍_정석/do-it-example/src/__tests__/04/Input.test.js
+++ book/리액트_프로그래밍_정석/do-it-example/src/__tests__/04/Input.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Input from '../../03/Input';
+import { shallow } from 'enzyme';
 
 describe('<Input>', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Input name='name' />, div);
-    ReactDOM.unmountComponentAtNode(div);
-    expect(React.isValidElement(<Input name='test_name' />)).toBeTruthy();
+    expect(() => {
+      shallow(<Input name='name' />);
+    }).not.toThrow();
+  });
+
+  it('has one element', () => {
+    const wrapper = shallow(<Input name={'test_name'} />);
+    expect(wrapper.length).toEqual(1);
+    expect(wrapper).toHaveLength(1);
   });
 });
