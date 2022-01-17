74038f1aa96fdfcd7318a9f405d634d376204a14
apeltop <sunshine@ptokos.com>
2022-01-17 17:17:19 +0900

    storybook-addon-jsx 사용 예제 추가

--- book/리액트_프로그래밍_정석/do-it-example/src/stories/InputStory.jsx
+++ book/리액트_프로그래밍_정석/do-it-example/src/stories/InputStory.jsx
@@ -4,6 +4,6 @@ import Input from '../03/Input';
 import { action } from '@storybook/addon-actions';
 
 storiesOf('Input', module)
-  .add('기본 설정', () => <Input name='name' />)
-  .add('label 예제', () => <Input name='name' label='이름' />)
-  .add('onChane 예제', () => <Input name='name' onChange={action('onChange 이벤트 발생')} />);
+  .addWithJSX('기본 설정', () => <Input name='name' />)
+  .addWithJSX('label 예제', () => <Input name='name' label='이름' />)
+  .addWithJSX('onChane 예제', () => <Input name='name' onChange={action('onChange 이벤트 발생')} />);
