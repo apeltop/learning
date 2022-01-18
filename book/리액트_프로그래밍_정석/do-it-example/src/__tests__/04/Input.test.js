79789a0335ab99effb77e24114b89c315b739922
apeltop <sunshine@ptokos.com>
2022-01-18 22:31:19 +0900

    enzyme find 메소드 예제 작성

--- book/리액트_프로그래밍_정석/do-it-example/src/__tests__/04/Input.test.js
+++ book/리액트_프로그래밍_정석/do-it-example/src/__tests__/04/Input.test.js
@@ -15,4 +15,12 @@ describe('<Input>', () => {
     expect(wrapper.length).toEqual(1);
     expect(wrapper).toHaveLength(1);
   });
+
+  describe('contains <input>', () => {
+    it('renders one input', () => {
+      const wrapper = shallow(<Input name='test_name' />);
+      expect(wrapper.find('input')).toHaveLength(1);
+      expect(wrapper.find('label')).toHaveLength(1);
+    });
+  });
 });
