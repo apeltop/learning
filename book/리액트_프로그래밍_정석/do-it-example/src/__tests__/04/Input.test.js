5120f1d050302da3091700f9fc9128afabe1ae61
apeltop <sunshine@ptokos.com>
2022-01-18 22:38:31 +0900

    enzyme setProps 예제 작성

--- book/리액트_프로그래밍_정석/do-it-example/src/__tests__/04/Input.test.js
+++ book/리액트_프로그래밍_정석/do-it-example/src/__tests__/04/Input.test.js
@@ -26,6 +26,20 @@ describe('<Input>', () => {
     expect(type).toBe('text');
   });
 
+  it('renders errorMessage', () => {
+    const wrapper = shallow(<Input name='test_name' />);
+    expect(wrapper.find('.eeror')).toHaveLength(0);
+
+    const expectedErrorMessage = '옳지 못한 값이 입력되었습니다.';
+    wrapper.setProps({
+      errorMessage: expectedErrorMessage,
+    });
+
+    expect(wrapper.find('span').prop('className')).toBe('error');
+    expect(wrapper.find('.error')).toHaveLength(1);
+    expect(wrapper.html()).toContain(expectedErrorMessage);
+  });
+
   describe('contains <input>', () => {
     it('renders one input', () => {
       const wrapper = shallow(<Input name='test_name' />);
