fc792f563c91579a5293dc54f4667558fc6a7df8
apeltop <sunshine@ptokos.com>
2022-01-19 20:18:08 +0900

    withError 예제 추가

---
+++ book/리액트_프로그래밍_정석/do-it-example/src/stories/WithErrorStory.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import withError from '../05/withError';
+import Input from '../04/InputWithStyles';
+
+const InputWithError = withError('올바르지 못한 값입니다.')(Input);
+
+storiesOf('WithError', module)
+  .addWithJSX('기본 설정', () => <InputWithError name='name' hasError />)
+  .addWithJSX('errorMessage 예제', () => <InputWithError name='name' hasError errorMessage={'필수 항목입니다.'} />)
