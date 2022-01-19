6a92716272683c31d4edefd8f5856e7ae2686707
apeltop <sunshine@ptokos.com>
2022-01-19 20:04:05 +0900

    lifecycle 예제에서 로딩 기능 추가

--- book/리액트_프로그래밍_정석/do-it-example/src/stories/LifecycleStory.jsx
+++ book/리액트_프로그래밍_정석/do-it-example/src/stories/LifecycleStory.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { PageWithLoadData } from '../05/lifecycle';
+import { PageWithLoadData, PageWithLoadDataAndLoading } from '../05/lifecycle';
 
 storiesOf('Lifecycle', module)
-  .addWithJSX('loadData 예제', () => <PageWithLoadData loadData={() => fetch('/').then(() => 'hello')} />);
+  .addWithJSX('loadData 예제', () => <PageWithLoadData loadData={() => fetch('/').then(() => 'hello')} />)
+  .addWithJSX('로딩 메세지 예제', () => <PageWithLoadDataAndLoading
+    loadData={() => {
+      return fetch('/').then(() => 'hello');
+    }} />);
