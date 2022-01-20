70b4aaee925f9a3c06727d992e457ab1246675c0
apeltop <sunshine@ptokos.com>
2022-01-20 14:02:30 +0900

    redux graphdb 데이터 수정 예제 작성

--- book/리액트_프로그래밍_정석/do-it-example/src/stories/ReduxStory.jsx
+++ book/리액트_프로그래밍_정석/do-it-example/src/stories/ReduxStory.jsx
@@ -5,10 +5,12 @@ import { ReduxApp as ReduxApp2 } from '../07/ReduxApp02';
 import { ReduxApp as ReduxApp3 } from '../07/ReduxApp03';
 import AdvReduxApp from '../07/AdvReduxApp03';
 import AdvReduxApp4 from '../07/AdvReduxApp04';
+import AdvReduxApp5 from '../07/AdvReduxApp05';
 
 storiesOf('Redux', module)
   .addWithJSX('기본 설정', () => <ReduxApp>안녕하세요</ReduxApp>)
   .addWithJSX('dispatch 예제', () => <ReduxApp2 />)
   .addWithJSX('reducer 예제', () => <ReduxApp3 />)
   .addWithJSX('그래프 데이터베이스 기본 예제', () => <AdvReduxApp />)
-  .addWithJSX('그래프 데이터베이스 데이터 읽기 예제', () => <AdvReduxApp4 />);
+  .addWithJSX('그래프 데이터베이스 데이터 읽기 예제', () => <AdvReduxApp4 />)
+  .addWithJSX('그래프 데이터베이스 데이터 수정 예제', () => <AdvReduxApp5 />);
