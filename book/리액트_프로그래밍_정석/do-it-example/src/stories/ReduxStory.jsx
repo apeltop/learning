9d59f98b3dbeaffd71001fad129b76e8fd42402d
apeltop <sunshine@ptokos.com>
2022-01-20 11:50:53 +0900

    redux dispatch 예제 작성

--- book/리액트_프로그래밍_정석/do-it-example/src/stories/ReduxStory.jsx
+++ book/리액트_프로그래밍_정석/do-it-example/src/stories/ReduxStory.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { ReduxApp } from '../07/ReduxApp01';
+import { ReduxApp as ReduxApp2 } from '../07/ReduxApp02';
 
 storiesOf('Redux', module)
-  .addWithJSX('기본 설정', () => <ReduxApp>안녕하세요</ReduxApp>);
+  .addWithJSX('기본 설정', () => <ReduxApp>안녕하세요</ReduxApp>)
+  .addWithJSX('dispatch 예제', () => <ReduxApp2 />);
