b62521c4215fcd5752b1db79df752da339663380
apeltop <sunshine@ptokos.com>
2022-01-20 13:51:32 +0900

    redux graphdb 예제 작성

---
+++ book/리액트_프로그래밍_정석/do-it-example/src/07/actions/collectionActions01.js
@@ -0,0 +1,7 @@
+export const SET_COLLECTION = 'collection/SET_COLLECTION';
+
+export const setCollection = collection => ({
+  type: SET_COLLECTION,
+  payload: collection,
+});
+
--- book/리액트_프로그래밍_정석/do-it-example/src/stories/ReduxStory.jsx
+++ book/리액트_프로그래밍_정석/do-it-example/src/stories/ReduxStory.jsx
@@ -3,8 +3,10 @@ import { storiesOf } from '@storybook/react';
 import { ReduxApp } from '../07/ReduxApp01';
 import { ReduxApp as ReduxApp2 } from '../07/ReduxApp02';
 import { ReduxApp as ReduxApp3 } from '../07/ReduxApp03';
+import AdvReduxApp from '../07/AdvReduxApp03';
 
 storiesOf('Redux', module)
   .addWithJSX('기본 설정', () => <ReduxApp>안녕하세요</ReduxApp>)
   .addWithJSX('dispatch 예제', () => <ReduxApp2 />)
-  .addWithJSX('reducer 예제', () => <ReduxApp3 />);
+  .addWithJSX('reducer 예제', () => <ReduxApp3 />)
+  .addWithJSX('그래프 데이터베이스 기본 예제', () => <AdvReduxApp />);
