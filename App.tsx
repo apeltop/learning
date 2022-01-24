c3bb3cb11b1429d9a6c97b9618b06460c6702f81
apeltop <sunshine@ptokos.com>
2022-01-24 14:39:35 +0900

    fake 데이터 생성하는 예제 작성

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import {SafeAreaView, Text} from 'react-native';
+import * as D from './src/data';
+
+const person = D.createRandomPerson();
 
 export default function App() {
     return (
         <SafeAreaView>
-            <Text>Hello JSX world!</Text>
+            <Text>{JSON.stringify(person, null, 2)}</Text>
         </SafeAreaView>
     );
 }
