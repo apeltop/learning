3535bde057345547487a2756997865bb4c9eec50
apeltop <sunshine@ptokos.com>
2023-12-11 19:45:35 +0900

    [book] 타입스크립트_리액트_NextJs로_배우는_실전_웹_애플리케이션_개발 개발 환경을 위한 rewrites 설정 추가하였다.

--- book/타입스크립트_리액트_NextJs로_배우는_실전_웹_애플리케이션_개발/ch5/next.config.js
+++ book/타입스크립트_리액트_NextJs로_배우는_실전_웹_애플리케이션_개발/ch5/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
     reactStrictMode: true,
     compiler: {
         styledComponents: true,
+    },
+    async rewrites() {
+        return [
+            {
+                source: `${process.env.NEXT_PUBLIC_API_URL}/:match*`,
+                destination: `${process.env.API_BASE_URL}/:match*`,
+            }
+        ]
     }
 }
 
