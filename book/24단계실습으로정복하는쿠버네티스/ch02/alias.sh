573c2b3fafb687c1ddbfabf1cf30e970aab1fcae
apeltop <sunshine@ptokos.com>
2024-09-19 23:03:59 +0900

    [book] 24단계실습으로정복하는쿠버네티스 ch02 alias 파일 추가하였다.

---
+++ book/24단계실습으로정복하는쿠버네티스/ch02/alias.sh
@@ -0,0 +1,15 @@
+cat <<EOF>> ~/.bashrc
+alias k='kubectl'
+alias ka='kubectl apply --recursive -f'
+alias kgp='kubectl get pods -o wide'
+alias kgd='kubectl get deploy -o wide'
+alias kgs='kubectl get service -o wide'
+alias kgn='kubectl get nodes -o wide'
+alias kge='kubectl get events -w --field-selector type=Warning'
+alias kgv='kubectl get pvc -o wide'
+alias kgpa='kubectl get pods -o wide -A'
+alias kgpw='kubectl get pods -o wide -w'
+alias kgpaw='kubectl get pods -o wide -A -w'
+alias kcns='kubectl create deploy netshoot --image nicolaka/netshoot -- sleep inf'
+alias kcb='kubectl run busybox --image=busybox --restart=Never -- sleep 1d'
+EOF
