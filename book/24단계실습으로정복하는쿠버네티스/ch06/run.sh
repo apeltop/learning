4f30afc5ceb395cfd753fcf05c59e54f4dbe6ed4
apeltop <sunshine@ptokos.com>
2024-09-22 13:32:21 +0900

    [book] 24단계실습으로정복하는쿠버네티스 ch06 예제 진행하였다.

---
+++ book/24단계실습으로정복하는쿠버네티스/ch06/run.sh
@@ -0,0 +1,22 @@
+helm earch repo nginx
+
+helm pull bitnami/nginx
+
+helm tar xvfz nginx-18.2.0.tgz
+
+rm -rf nginx-18.2.0.tgz
+
+mv nginx nginx-18.2.0
+
+cp values.yaml my-values.yaml
+
+kubectl create ns nginx
+
+kubectl ns nginx
+
+helm install nginx -f my-values.yaml .
+
+helm ls
+
+helm get manifest nginx
+
