5b7edc1d631523352b5939d1fcd72dae4ea23230
apeltop <sunshine@ptokos.com>
2024-09-28 18:13:30 +0900

    [book] 24단계실습으로정복하는쿠버네티스 ch02 에 예제 추가하였다.

---
+++ book/24단계실습으로정복하는쿠버네티스/ch02/run.sh
@@ -0,0 +1,32 @@
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'complete -F __start_kubectl k' >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt -y install bash-completion
+type _init_completion
+echo 'source <(kubectl completion bash)' >> ~/.bashrc
+echo 'alias k=kubectl' >> ~/.bashrc
+echo 'complete -F __start_kubectl k' >> ~/.bashrc
+source ~/.bashrc
+
+# krew install
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+kubectl krew install ctx
+kubectl krew install ns
+
+
+git clone https://github.com/jonmosco/kube-ps1.git
+source ~/.bashrc
+nano ~/.bashrc
