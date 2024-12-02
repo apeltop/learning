c4de0cf9139e35c8cfcd7a3f5c1d11c41ef10b1e
apeltop <sunshine@ptokos.com>
2024-12-02 13:30:13 +0900

    [algorithm] 프로그래머스 코딩 테스트 12913 문제 코드 추가하였다.

---
+++ algorithm/programmers/12913.py
@@ -0,0 +1,30 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12913
+
+def solution(land):
+    for i in range(0, len(land) - 1):
+        row = land[i]
+
+        order_row = sorted(row)[::]
+        max_1, max_2 = order_row[-1], order_row[-2]
+        max_1_i, max_2_i = row.index(max_1), row.index(max_2)
+        indexes = [0, 1, 2, 3]
+        indexes.remove(max_1_i)
+
+        for index in indexes:
+            land[i + 1][index] += max_1
+        land[i + 1][max_1_i] += max_2
+
+    return max(land[-1])
+
+
+# print(solution([
+#     [1, 2, 3, 5],
+#     [5, 6, 7, 8],
+#     [4, 3, 2, 1]]),
+#     16)
+
+print(solution([
+    [1, 2, 3, 5],
+    [5, 6, 7, 8],
+    [4, 3, 2, 100]]),
+    112)
